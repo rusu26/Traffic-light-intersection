71535bf1679fea08a1332f6be95f2138ffe5a7d7
Rusu Vasile-Narcis-Mihai <82117727+rusu26@users.noreply.github.com>
2023-03-06 20:21:36 +0200

    Update Traffic_light_intersection.c

--- Source Code/Traffic_light_intersection.c
+++ Source Code/Traffic_light_intersection.c
@@ -1,26 +1,27 @@
-// Definesc F_CPU inainte de a include delay.h
+//To understand the variables translate from Romanian to your language.
+// I define F_CPU before including delay.h
 #define F_CPU 2000000UL
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-// Definesc Stare
+// Define Stats
 struct Stare {
 unsigned char Iesire;
 unsigned char Afisor;
 unsigned long Timp;
-// Urmatoarea este un pointer catre urmatoarele stari din starea curenta
+// Next is a pointer to the next states from the current state
 struct Stare *Urmatoarea[4];
 };
-// Typedef catre struct Stare -> TipStare pentru a utiliza mai jos in AUTOMAT.
+// Typedef to struct Status -> TypeState to use below in AUTOMAT.
 typedef struct Stare TipStare;
 
-/* definirea starilor-sunt adresele celor 4 stari*/
+/* definition of the states - are the addresses of the 4 states*/
 #define treciN &AUTOMAT[0]
 #define staiN  &AUTOMAT[1]
 #define treciE &AUTOMAT[2]
 #define staiE  &AUTOMAT[3]
 
-/*Automatul nostru*/
+/*Our vending machine*/
 TipStare AUTOMAT[4]={
 {0b00100001,0x54, 5000,{treciN,staiN,treciN,staiN}},
 {0b00100010,0x40, 1000,{treciE,treciE,treciE,treciE}},
@@ -29,24 +30,23 @@ TipStare AUTOMAT[4]={
 };
 
 int main(void){
-TipStare *Pointer;//pointerul de stare
+TipStare *Pointer;//the status pointer
 unsigned char Intrare;
-/* initializare porturi*/
-DDRA = DDRA & 0xFC; //PA1 PA0 intrari
-PORTA = PORTA | 0x03; //activam pe intrari rez.interne de pull up
-DDRC = DDRC | 0x3F; //PC5..PC0 iesiri
-PORTC = PORTC & 0xE0; //LED uri initial stinse
-DDRD= 0xFF; //Setam porturile PD ca iesiri
-PORTD= 0x00; //Setam LED-urile initial stinse
+/* ports initialization*/
+DDRA = DDRA & 0xFC; //PA1 PA0 inputs
+PORTA = PORTA | 0x03; //we activate on internal res. pull up inputs
+DDRC = DDRC | 0x3F; //PC5..PC0 output
+PORTC = PORTC & 0xE0; //LEDs initially off
+DDRD= 0xFF; //Set PD ports as outputs
+PORTD= 0x00; //Set the LEDs initially off
 
-Pointer =treciN;//pointer pentru starea initiala
+Pointer =treciN;//Pointer for the initial state
 
 while(1){
-PORTC = Pointer->Iesire;//actualizam iesire
+PORTC = Pointer->Iesire;//update the output
 PORTD = Pointer->Afisor;
-_delay_ms(Pointer-> Timp);//intarzierea corespunzatoare starii
-Intrare = PINA& 0x03;//citire senzori, port A
-//INCERCAREA DE A PUNE CONDITII PENTRU A SE APRINDE DOAR PENTRU TRECI E SI TRECI E AUTOMATE PE POZITIA 2 SI 4
-Pointer = Pointer-> Urmatoarea[Intrare];//trecerea la urmatoarea stare
+_delay_ms(Pointer-> Timp);//the delay corresponding to the state
+Intrare = PINA& 0x03;//read sensors port A
+Pointer = Pointer-> Urmatoarea[Intrare];//pass to the next state
 }
 }
